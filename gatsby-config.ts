141d0f13ba3bb1a10b215f8c2321b3824d8f9a8a
Oste Jannick <jannick.oste@ap.be>
2023-01-07 13:43:15 +0100

    polyfill changed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,9 +6,6 @@ const config: GatsbyConfig = {
     siteUrl: `https://gatsbyschool.oste.dev`,
     github: "jannickoste"
   },
-  flags: {
-    DEV_SSR: true
-  },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
--- src/components/layout/MasterPage.tsx
+++ src/components/layout/MasterPage.tsx
@@ -45,9 +45,9 @@ export const MasterPage = ({children}: {children:((styling: ThemeStyling) => JSX
     const onThemeSwitch = () => {
         const newMode = !state.darkMode;
         setState({...state, darkMode: newMode})
-        let locStorage = typeof window === undefined ? require("localstorage-polyfill") : localStorage;
+        let locStorage = typeof window === undefined ? new (require("localstorage-polyfill"))() : localStorage;
         
-        localStorage.setItem("dark", newMode  ? "1" : "0");
+        locStorage.setItem("dark", newMode  ? "1" : "0");
     }
     
     return (
